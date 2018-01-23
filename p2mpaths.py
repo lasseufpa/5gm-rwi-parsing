a97300f9cb073dc179c3b2ba19746b383d651fd7
Felipe Bastos <felipe.bastos@ymail.com>
2018-01-23 15:36:53 -0300

    add gets

--- p2mpaths.py
+++ p2mpaths.py
@@ -55,18 +55,15 @@ class P2MDoA():
         return self.data
         
     def _parse_meta(self):
-        print ("meta 1")
         match = re.match(P2MDoA._filename_match_re,
                          os.path.basename(self.filename))
         self.project = match.group('project')
-        print ("meta 2")
         self.transmitter_set = int(match.group('transmitter_set'))
         self.transmitter = int(match.group('transmitter'))
         self.receiver_set = int(match.group('receiver_set'))
         
     def _parse(self):
         with open(self.filename) as self.file:
-            print ("file opened")
             self._parse_meta()
             self._parse_header()
             self.data = collections.OrderedDict()
@@ -109,23 +106,6 @@ class P2MPaths(P2MDoA):
     """Parse a p2m paths file
     > P2MPaths('iter0.paths.t001_05.r006.p2m').get_data_ndarray()
     """
-    # project.type.tx_y.rz.p2m
-    _filename_match_re = (r'^(?P<project>.*)' +
-                          '\.' + 
-                          'paths' + 
-                          '\.' + 
-                          't(?P<transmitter>\d+)'+
-                          '_' +
-                          '(?P<transmitter_set>\d+)' +
-                          '\.' + 
-                          'r(?P<receiver_set>\d+)' + 
-                          '\.' +
-                          'p2m$')
-
-    #def __init__(self, filename):
-     #   super(P2MPaths, self).__init__(filename)
-
-
 
     def _parse_receiver(self):
         '''Get receiver and number of paths(pair Tx-Rx)'''
@@ -139,8 +119,9 @@ class P2MPaths(P2MDoA):
         self.data[receiver]['received_power'] = received_power
         self.data[receiver]['arrival_time'] = arrival_time
         self.data[receiver]['spread_delay'] = spread_delay
-        print (self.data) #debug
-        print (self.data[receiver]) #debug
+        self.data[receiver]['paths_number'] = n_paths
+        #print (self.data) #debug
+        #print (self.data[receiver]) #debug
         #for rec in range(self.n_receivers):
         '''Read: srcvdpower, arrival_time, arrival_angle1, arrival_angle2, departure_angle1, departure_angle2
             GETTING ERROR --- TO FIX'''
@@ -148,7 +129,7 @@ class P2MPaths(P2MDoA):
             line = self._get_next_line()
             ray_n, n_interactions, srcvdpower, arrival_time, arrival_angle1, arrival_angle2, departure_angle1, departure_angle2 = [float(i) for i in line.split()]
             interactions_list = self._get_next_line().strip()
-            print (interactions_list) #debug
+            #print (interactions_list) #debug
             ray_n = int(ray_n)
             n_interactions = int(n_interactions)
             self.data[receiver][ray_n] = collections.OrderedDict()
@@ -162,29 +143,71 @@ class P2MPaths(P2MDoA):
             self.data[receiver][ray_n]['interactions'] = collections.OrderedDict()
             
             #print (self.data)
-            print(self.data[receiver])
-            print(interactions_list.split('-')[0])
+            #print(self.data[receiver])
+            #print(interactions_list.split('-')[0])
             #print(self.data[receiver][ray][interactions_list])
             '''Get coordenates of interactions'''
             for i in range(n_interactions+2):
                 line = self._get_next_line()
                 sp_line = line.split()
-                print (sp_line)
+                #print (sp_line)
                 interaction = i 
-                print (interaction)
+                #print (interaction)
                 coordenates = np.array([float(j) for j in sp_line[0:]])
                 self.data[receiver][ray_n]['interactions'][interactions_list.split('-')[i]] = coordenates
-                print(self.data[receiver][ray_n]['interactions'])
-            print(self.data)
+                #print(self.data[receiver][ray_n]['interactions'])
+            #print(self.data)
 
+    def get_departure_angle_ndarray(self, antenna_number):
+
+        ''' return the daparture angles as a ndarray
+        
+        The array is shaped (number_paths, departure_angle1, departure_angle2)
+        '''
+        data_ndarray = np.zeros((self.data[antenna_number]['paths_number'], 2))
+        for paths in xrange(self.data[antenna_number]['paths_number']):
+            #print(self.data[antenna_number][paths+1]['srcvdpower'])
+            data_ndarray[paths][0]= self.data[antenna_number][paths+1]['departure_angle1']
+            data_ndarray[paths][1]= self.data[antenna_number][paths+1]['departure_angle2']
+            pass
+        return data_ndarray
+    def get_arrival_angle_ndarray(self, antenna_number):
+
+        ''' return the daparture angles as a ndarray
+        
+        The array is shaped (number_paths, arrival_angle1, arrival_angle2)
+        '''
+        data_ndarray = np.zeros((self.data[antenna_number]['paths_number'], 2))
+        for paths in xrange(self.data[antenna_number]['paths_number']):
+            data_ndarray[paths][0]= self.data[antenna_number][paths+1]['arrival_angle1']
+            data_ndarray[paths][1]= self.data[antenna_number][paths+1]['arrival_angle2']
+            pass
+        return data_ndarray
+
+    def get_p_gain_ndarray(self, antenna_number):
+
+        ''' return the daparture angles as a ndarray
+        
+        The array is shaped (number_paths, arrival_angle1, arrival_angle2)
+        '''
+        data_ndarray = np.zeros((self.data[antenna_number]['paths_number'], 1))
+        for paths in xrange(self.data[antenna_number]['paths_number']):
+            data_ndarray[paths][0]= self.data[antenna_number][paths+1]['srcvdpower']
+            pass
+        return data_ndarray
 
 
 
 if __name__=='__main__':
     path = P2MPaths('example/iter0.paths.t001_05.r006.p2m')
-    doa = P2MDoA('iter0.doa.t001_05.r006.p2m')
-    print('project: ', doa.project)
-    print('transmitter set: ', doa.transmitter_set)
-    print('transmitter: ', doa.transmitter)
-    print('receiver set: ', doa.receiver_set)
-    print(doa.get_data_ndarray())
+    doa = P2MDoA('example/iter0.doa.t001_05.r006.p2m')
+    #print(path.receiver_set)
+    #print('project: ', doa.project)
+    #print('transmitter set: ', doa.transmitter_set)
+    #print('transmitter: ', doa.transmitter)
+    #print('receiver set: ', doa.receiver_set)
+    #print(doa.get_data_ndarray())
+    #print(path.data[1])
+    print('Departure angeles: ',path.get_departure_angle_ndarray(1))
+    print('Arrival angeles: ',path.get_arrival_angle_ndarray(1))
+    print('Gains: ',path.get_p_gain_ndarray(1))
