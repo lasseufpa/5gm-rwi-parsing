20860ab790f6a0a154115132cb2d9327abdf428a
Pedro Batista <pedosb@gmail.com>
2018-01-23 17:26:37 -0300

    parcial Felipe

--- p2mdoa.py
+++ p2mdoa.py
@@ -4,10 +4,76 @@ import collections
 
 import numpy as np
 
+
 class ParsingError(Exception):
     pass
 
-class P2MDoA():
+
+class P2mFileParser:
+    """Parser for p2m files"""
+
+    # project.type.tx_y.rz.p2m
+    _filename_match_re = (r'^(?P<project>.*)' +
+                          r'\.' +
+                          r'(?P<type>((doa)|(paths)))' +
+                          r'\.' +
+                          r't(?P<transmitter>\d+)'+
+                          r'_' +
+                          r'(?P<transmitter_set>\d+)' +
+                          r'\.' +
+                          r'r(?P<receiver_set>\d+)' +
+                          r'\.' +
+                          r'p2m$')
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = None
+        self._parse()
+
+    def get_data_dict(self):
+        return self.data
+
+    def _parse_meta(self):
+        match = re.match(P2mFileParser._filename_match_re,
+                         os.path.basename(self.filename))
+        self.project = match.group('project')
+        self.transmitter_set = int(match.group('transmitter_set'))
+        self.transmitter = int(match.group('transmitter'))
+        self.receiver_set = int(match.group('receiver_set'))
+
+    def _parse(self):
+        with open(self.filename) as self.file:
+            self._parse_meta()
+            self._parse_header()
+            self.data = collections.OrderedDict()
+            for rec in range(self.n_receivers):
+                self._parse_receiver()
+
+    def _parse_header(self):
+        """read the first line of the file, indicating the number of receivers"""
+        line = self._get_next_line()
+        self.n_receivers = int(line.strip())
+
+    def _parse_receiver(self):
+        raise NotImplementedError()
+
+    def _get_next_line(self):
+        """Get the next uncommedted line of the file
+
+        Call this only if a new line is expected
+        """
+        if self.file is None:
+            raise ParsingError('File is closed')
+        while True:
+            next_line = self.file.readline()
+            if next_line == '':
+                raise ParsingError('Unexpected end of file')
+            if re.search(r'^\s*#', next_line, re.DOTALL):
+                continue
+            else:
+                return next_line
+
+class P2MDoA(P2mFileParser):
     """Parse a p2m direction of arrival file
     > P2MDoA('iter0.doa.t001_05.r006.p2m').get_data_ndarray()
     """
@@ -50,31 +116,7 @@ class P2MDoA():
             if n_paths > biggest:
                 biggest = n_paths
         return biggest
-    
-    def get_data_dict(self):
-        return self.data
-        
-    def _parse_meta(self):
-        match = re.match(P2MDoA._filename_match_re,
-                         os.path.basename(self.filename))
-        self.project = match.group('project')
-        self.transmitter_set = int(match.group('transmitter_set'))
-        self.transmitter = int(match.group('transmitter'))
-        self.receiver_set = int(match.group('receiver_set'))
-        
-    def _parse(self):
-        with open(self.filename) as self.file:
-            self._parse_meta()
-            self._parse_header()
-            self.data = collections.OrderedDict()
-            for rec in range(self.n_receivers):
-                self._parse_receiver()
 
-    def _parse_header(self):
-        '''read the first line of the file, indicating the number of receivers'''
-        line = self._get_next_line()
-        self.n_receivers = int(line.strip())
-        
     def _parse_receiver(self):
         line = self._get_next_line()
         receiver, n_paths = [int(i) for i in line.split()]
@@ -85,22 +127,6 @@ class P2MDoA():
             path = int(sp_line[0])
             direction = np.array([float(j) for j in sp_line[1:]])
             self.data[receiver][path] = direction
-        
-    def _get_next_line(self):
-        '''get the next uncommedted line of the file
-        
-        Call this only if a new line is expected
-        '''
-        if self.file is None:
-            raise ParsingError('File is closed')
-        while True:
-            next_line = self.file.readline()
-            if next_line == '':
-                raise ParsingError('Unexpected end of file')
-            if re.search(r'^\s*#', next_line, re.DOTALL):
-                continue
-            else:
-                return next_line
 
 if __name__=='__main__':
     doa = P2MDoA('example/iter0.doa.t001_05.r006.p2m')
--- p2mpaths.py
+++ p2mpaths.py
@@ -4,111 +4,14 @@ import collections
 
 import numpy as np
 
-class ParsingError(Exception):
-    pass
+from p2mdoa import P2mFileParser
 
-class P2MDoA():
-    """Parse a p2m direction of arrival file
-    > P2MDoA('iter0.doa.t001_05.r006.p2m').get_data_ndarray()
-    """
-    # project.type.tx_y.rz.p2m
-    _filename_match_re = (r'^(?P<project>.*)' +
-                          '\.' + 
-                          '(paths|doa)' + 
-                          '\.' + 
-                          't(?P<transmitter>\d+)'+
-                          '_' +
-                          '(?P<transmitter_set>\d+)' +
-                          '\.' + 
-                          'r(?P<receiver_set>\d+)' + 
-                          '\.' +
-                          'p2m$')
-    
-    def __init__(self, filename):
-        self.filename = filename
-        self.file = None
-        self._parse()
-        
-    def get_data_ndarray(self):
-        ''' return the DoA as a ndarray
-        
-        The array is shaped (reiceiver, path, direction) the order is the one they appear in the file
-        
-        If a receiver has less paths than another its path is populated with zeros
-        '''
-        data_ndarray = np.zeros((self.n_receivers, self.biggest_n_paths(), 3))
-        for rec_idx, path_dict in enumerate(self.data.values()):
-            for path_idx, direction in enumerate(path_dict.values()):
-                data_ndarray[rec_idx][path_idx][:] = direction
-        return data_ndarray
-    
-    def biggest_n_paths(self):
-        ''' find the reciever with the biggest number of received paths'''
-        biggest = -np.inf
-        for receiver, receiver_v in self.data.items():
-            n_paths = len(receiver_v)
-            if n_paths > biggest:
-                biggest = n_paths
-        return biggest
-    
-    def get_data_dict(self):
-        return self.data
-        
-    def _parse_meta(self):
-        match = re.match(P2MDoA._filename_match_re,
-                         os.path.basename(self.filename))
-        self.project = match.group('project')
-        self.transmitter_set = int(match.group('transmitter_set'))
-        self.transmitter = int(match.group('transmitter'))
-        self.receiver_set = int(match.group('receiver_set'))
-        
-    def _parse(self):
-        with open(self.filename) as self.file:
-            self._parse_meta()
-            self._parse_header()
-            self.data = collections.OrderedDict()
-            for rec in range(self.n_receivers):
-                self._parse_receiver()
 
-    def _parse_header(self):
-        '''read the first line of the file, indicating the number of receivers'''
-        line = self._get_next_line()
-        self.n_receivers = int(line.strip())
-        
-    def _parse_receiver(self):
-        line = self._get_next_line()
-        receiver, n_paths = [int(i) for i in line.split()]
-        self.data[receiver] = collections.OrderedDict()
-        for i in range(n_paths):
-            line = self._get_next_line()
-            sp_line = line.split()
-            path = int(sp_line[0])
-            direction = np.array([float(j) for j in sp_line[1:]])
-            self.data[receiver][path] = direction
-        
-    def _get_next_line(self):
-        '''get the next uncommedted line of the file
-        
-        Call this only if a new line is expected
-        '''
-        if self.file is None:
-            raise ParsingError('File is closed')
-        while True:
-            next_line = self.file.readline()
-            if next_line == '':
-                raise ParsingError('Unexpected end of file')
-            if re.search(r'^\s*#', next_line, re.DOTALL):
-                continue
-            else:
-                return next_line
-
-class P2MPaths(P2MDoA):
-    """Parse a p2m paths file
-    > P2MPaths('iter0.paths.t001_05.r006.p2m').get_data_ndarray()
-    """
+class P2mPaths(P2mFileParser):
+    """Parse a p2m paths file"""
 
     def _parse_receiver(self):
-        '''Get receiver and number of paths(pair Tx-Rx)'''
+        """Get receiver and number of paths (pair Tx-Rx)"""
         line = self._get_next_line()
         receiver, n_paths = [int(i) for i in line.split()]
         '''Read: received_power -self.data[receiver][0]-, arrival_time -self.data[receiver][1]-,
@@ -159,11 +62,10 @@ class P2MPaths(P2MDoA):
             #print(self.data)
 
     def get_departure_angle_ndarray(self, antenna_number):
-
-        ''' return the daparture angles as a ndarray
+        """ return the daparture angles as a ndarray
         
         The array is shaped (number_paths, departure_angle1, departure_angle2)
-        '''
+        """
         data_ndarray = np.zeros((self.data[antenna_number]['paths_number'], 2))
         for paths in xrange(self.data[antenna_number]['paths_number']):
             #print(self.data[antenna_number][paths+1]['srcvdpower'])
