8fb417ec7ce9ba4bc61a5a9e3839f260651430f2
aldebaro <aldebaro.klautau@gmail.com>
2018-08-07 19:12:52 -0500

    Pointed to new URL

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='rwiparsing',
-      version='1.1',
+      version='1.2',
       description='Parsing of Remcom Wireless InSite .p2m files',
       author='LASSE',
       author_email='pedosb@gmail.com',
-      url='https://gitlab.lasse.ufpa.br/software/python-machine-learning/rwi-parsing',
+      url='https://github.com/lasseufpa/5gm-rwi-simulation',
       packages=['rwiparsing'],
       requires=['numpy(>=1.14)']
       )
